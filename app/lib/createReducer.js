85b751711ab261943df8826f78f880fb5ba500cc
Yisheng Cai <cai.eason@gmail.com>
2016-12-01 16:49:24 -0500

    Add code for reducer-creator

--- app/lib/createReducer.js
+++ app/lib/createReducer.js
@@ -1,1 +1,12 @@
 // Export reducer creator
+const createReducer = (initialState, handlers) => {
+  return (state = initialState, action) => {
+    if (handlers.hasOwnProperty(action.type)) {
+      return handlers[action.type](state, action);
+    } else {
+      return state;
+    }
+  }
+};
+
+export default createReducer;
