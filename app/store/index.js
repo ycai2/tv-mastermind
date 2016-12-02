c1fe0ad91f92c37fc5007cc81f55139decc4bd77
Yisheng Cai <cai.eason@gmail.com>
2016-12-01 22:11:37 -0500

    Add thunk and logger to store

--- app/store/index.js
+++ app/store/index.js
@@ -1,15 +1,24 @@
-// Export store
-
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import RootReducer from '../reducers';
 import RootMiddleware from '../middleware';
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__});
+
+const configureStore = (preloadedState = {}) => {
+  const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+
+  return createStore(
     RootReducer,
     preloadedState,
-    RootMiddleware
+    enhancer
   )
-);
+};
 
 export default configureStore;
--- index.ios.js
+++ index.ios.js
@@ -1,50 +1,14 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 // import AppContainer from './app/containers/AppContainer';
 import { Provider } from 'react-redux';
+import reducer from './app/reducers';
 import configureStore from './app/store';
 
 
 const store = configureStore();
-
-export default class TVMastermind extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to TV Mastermind!
-        </Text>
-        <Text style={styles.detail}>
-          TV Mastermind is a mobile app to provide TV show schedules & info
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  detail: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 const App = () => (
   <Provider store={store}>
