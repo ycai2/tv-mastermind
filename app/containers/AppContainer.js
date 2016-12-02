d56675001d2c54cddfdc16679bc987d11cc98d1f
Yisheng Cai <cai.eason@gmail.com>
2016-12-01 22:32:46 -0500

    Test out normal react component behaviors with touchable highlight

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -7,15 +7,29 @@ import { ActionCreators } from '../actions';
 const {
   View,
   Text,
+  TouchableHighlight,
 } = ReactNative;
 
 class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showCount: 0};
+  }
+
+  incrementShowCount() {
+    this.setState({showCount: this.state.showCount + 1});
+  }
+
   render() {
     return (
       <View>
+
         <Text style={{marginTop: 20}}>
-          This is the app container for TV Mastermind.
+          This is the app container for TV Mastermind. Count: {this.state.showCount}
         </Text>
+        <TouchableHighlight onPress={() => {this.incrementShowCount()}}>
+          <Text>Add Count</Text>
+        </TouchableHighlight>
       </View>
     );
   }
