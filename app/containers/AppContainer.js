fa160098600b86d7bed0c0f2d2dd209e596770c7
Yisheng Cai <cai.eason@gmail.com>
2016-12-03 15:30:36 -0500

    Create home component

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -3,22 +3,12 @@ import ReactNative from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ActionCreators } from '../actions';
-
-const {
-  View,
-  Text,
-  TouchableHighlight,
-  ListView
-} = ReactNative;
+import Home from '../components/Home';
 
 class AppContainer extends Component {
   render() {
     return (
-      <View>
-        <Text style={{marginTop: 20}}>
-          This is the app container for TV Mastermind.
-        </Text>
-      </View>
+      <Home />
     );
   }
 }
